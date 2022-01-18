92a0b60227e332535d97cae2df3e3794f2513ee4
Ehsan Moradi <e.moradi274@gmail.com>
2022-01-18 18:57:01 +0330

    general info screen UI

--- lib/general_info/general_info_screen.dart
+++ lib/general_info/general_info_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:hamyar/overal_widgets/title_bar.dart';
-import 'package:hamyar/notes/notes_list.dart';
 
 class GeneralInfoScreen extends StatelessWidget {
   static const routeName = '/general-info-screen';
@@ -13,6 +12,7 @@ class GeneralInfoScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
     final MaterialColor? color = data['color'];
     final String title = data['title'];
+    final deviceSize = MediaQuery.of(context).size;
     return Theme(
       data: ThemeData(
         primarySwatch: color,
@@ -27,19 +27,129 @@ class GeneralInfoScreen extends StatelessWidget {
                   hasBackOption: true,
                 ),
                 const SizedBox(height: 20),
-                const Expanded(
+                Expanded(
                   child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: NotesList(),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: deviceSize.height * 0.2,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.primary,
+                                borderRadius: const BorderRadius.only(
+                                  bottomRight: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                ),
+                              ),
+                              alignment: Alignment.center,
+                              child: RotatedBox(
+                                quarterTurns: 1,
+                                child: Text(
+                                  'THIS MONTH',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Theme.of(context).canvasColor,
+                                    letterSpacing: 2,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: deviceSize.height * 0.08,
+                                  child: Text('chart'),
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Presence'),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: deviceSize.height * 0.08,
+                                  child: Text('chart'),
+                                ),
+                                const SizedBox(height: 10),
+                                const Text('Payments'),
+                              ],
+                            ),
+                            SizedBox(width: deviceSize.width * 0.02),
+                          ],
+                        ),
+                        const Divider(
+                          indent: 30,
+                          endIndent: 30,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              height: deviceSize.height * 0.35,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.primary,
+                                borderRadius: const BorderRadius.only(
+                                  bottomRight: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                ),
+                              ),
+                              alignment: Alignment.center,
+                              child: RotatedBox(
+                                quarterTurns: 1,
+                                child: Text(
+                                  'LAST 12 MONTHS',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Theme.of(context).canvasColor,
+                                    letterSpacing: 2,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                height: deviceSize.height * 0.35,
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Text(
+                                      'presence chart',
+                                      style: TextStyle(
+                                        color: Theme.of(context).canvasColor,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      'payments chart',
+                                      style: TextStyle(
+                                        color: Theme.of(context).canvasColor,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            // todo: make this button invisible on scrolling down
-            child: const Icon(Icons.add),
-            onPressed: () {},
           ),
         ),
       ),
